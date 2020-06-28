f44fb0615d21efa8fd78af111eb54a1e5ffe67d7
Frostack <soulburn007@gmail.com>
2020-06-29 01:02:15 +0430

    ✨ add api config

---
+++ src/apis/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:4000/',
+});
+
+instance.interceptors.request.use(
+  config => {
+    const token = Cookies.get('token');
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
