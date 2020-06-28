5586e5ebe98aa01eb602d4a3231bf9b7be142252
Frostack <soulburn007@gmail.com>
2020-06-29 01:52:12 +0430

    ✨ add TopicList component

---
+++ src/components/TopicList/TopicList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from './TopicList.module.css';
+
+function TopicList() {
+  return (
+    <div className={styles.TopicList}>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+      <p>Topic</p>
+    </div>
+  );
+}
+
+export default TopicList;
