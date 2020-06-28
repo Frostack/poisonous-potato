9fa7403f85501667a0ac2366fcb7f998772f5103
Frostack <soulburn007@gmail.com>
2020-06-29 01:03:00 +0430

    ✨ add App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
+import { verifyUser, connectSocket } from '../../actions';
 import styles from './App.module.css';
+import Header from '../Header/Header';
+import TopicList from '../TopicList/TopicList';
+import ChatSection from '../ChatSection/ChatSection';
+import InputSection from '../InputSection/InputSection';
+import Modal from '../Modal/Modal';
 
-function App() {
-  return <div className={styles.App}></div>;
+function App({ verifyUser, connectSocket }) {
+  useEffect(() => {
+    verifyUser();
+    connectSocket();
+  }, [verifyUser, connectSocket]);
+
+  return (
+    <div className={styles.App}>
+      <Header />
+      <TopicList />
+      <ChatSection />
+      <InputSection />
+      <Modal />
+    </div>
+  );
 }
 
-export default App;
+export default connect(null, { verifyUser, connectSocket })(App);
